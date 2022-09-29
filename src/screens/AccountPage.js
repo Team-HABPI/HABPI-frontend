df9805c20967c675aed36965259a7c8072eb9ea2
aashkul <aashish.kulkarni33@gmail.com>
2022-09-30 12:21:30 +1300

    Changes made on AccountPage.js & MainPage.js

--- src/screens/AccountPage.js
+++ src/screens/AccountPage.js
@@ -1,7 +1,76 @@
 // Library imports
-import { useState, useEffect } from "react";
+import { useState, useEffect, Image } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Box, Container, flexbox } from '@mui/system';
+import { Card, Grid } from "@mui/material";
+
+function Item1() {
+    return (
+
+        <Card sx={{
+            flexGrow: 1, color: 'blue', backgroundColor: 'yellow',
+
+            container: {
+
+                gap: 10,
+                rowGap: 10,
+                columnGap: 20,
+
+            },
+
+        }}> <h2> Item 1 </h2> </Card>
+
+
+    );
+}
+function Item2() {
+    const container1 = {
+        justifyContent: "flex-start",
+        gap: 10,
+        rowGap: 10,
+        columnGap: 70,
+        flexGrow: 1,
+        color: 'white',
+        backgroundColor: 'brown'
+    }
+
+    const container2 = {
+        justifyContent: "flex-start",
+        gap: 10,
+        rowGap: 50,
+        innerWidth: 50,
+        flexGrow: 1, color: 'white',
+        backgroundColor: 'green'
+    }
+
+    return (
+        <Container>
+            <Card sx={container1}> <h2> Pet Details</h2>
+                <p> <b>Name:</b> abcd </p>
+                <p> Animal type: dog </p>
+                <p> Age: 3 yeards </p>
+                <p> Gender: male </p>
+            </Card>
+
+            <Card sx={container2}> <h2> Pet Details</h2>
+                <p> <b>Name:</b> abcd </p>
+                <p> Animal type: dog </p>
+                <p> Age: 3 yeards </p>
+                <p> Gender: male </p>
+            </Card>
+
+        </Container>
+        // <Box sx={container}> <h2> Pet Details</h2>
+        //     <p> <b>Name:</b> abcd </p>
+        //     <p> Animal type: dog </p>
+        //     <p> Age: 3 yeards </p>
+        //     <p> Gender: male </p>
+        // </Box>
+
+    );
+}
+
 
 const AccountPage = () => {
     const [loadedUser, setLoadedUser] = useState();
@@ -20,17 +89,52 @@ const AccountPage = () => {
             });
     }, [userId]);
 
+
+    const container1 = {
+        justifyContent: "flex-start",
+        gap: 10,
+        rowGap: 10,
+        columnGap: 70,
+        flexGrow: 1,
+        color: 'black',
+        backgroundColor: 'silver'
+    }
+
+    const container2 = {
+        justifyContent: "flex-start",
+        gap: 10,
+        rowGap: 50,
+        flexGrow: 0.8,
+        color: 'black',
+        backgroundColor: 'silver'
+    }
+
     return (
-        <>
-            <h1>I am the Account Page</h1>
+
+        <Grid container spacing={2}>
+            <h1> Account Page</h1>
+
+            {loadedUser && (
+
+
+                <Card sx={{ p: 3, m: 10, alignItems: 'center' }}>
+
+                    <h1> {loadedUser.name}  </h1>
+
+                </Card>
+            )}
+
+
             {!loadedUser && <p>Loading or error</p>}
             {loadedUser && (
-                <>
-                    <p>Name: {loadedUser.name}</p>
-                    <p>Email: {loadedUser.email}</p>
+                <Card sx={{ p: 3, m: 10 }}>
+                    <h2><u> Person Details </u></h2>
+
                     <p>Age: {loadedUser.age}</p>
                     <p>Gender: {loadedUser.gender}</p>
-                    <h1>Pets</h1>
+                    <p>Email: {loadedUser.email}</p>
+
+                    {/* <h1>Pets</h1>
                     {loadedUser.pets.map((pet) => {
                         return (
                             <>
@@ -40,10 +144,37 @@ const AccountPage = () => {
                             </>
                         );
                     })}
-                    <p>Services: {loadedUser.services}</p>
-                </>
+
+                    <p>Services: {loadedUser.services}</p> */}
+                </Card>
             )}
-        </>
+
+            {loadedUser && (
+                <Card sx={{ p: 3, m: 10, backgroundColor: "silver", color: 'black' }}>
+                    <h2><u>Animals</u></h2>
+                    {loadedUser.pets.map((pet) => {
+                        return (
+                            <Card sx={{ p: 3, m: 8 }}>
+                                <h2 key={pet.id}>{pet.name}</h2>
+                                <p key={pet.id}>Age: {pet.age}</p>
+                                <p key={pet.id}>Breed: {pet.breed}</p>
+                                <p>Services: {loadedUser.services}</p>
+                            </Card>
+                        );
+                    })}
+
+                </Card>
+
+
+            )
+            }
+
+            {/* <Item1 />
+            <Item2 /> */}
+        </Grid >
+
+
+
     );
 };
 
--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -1,5 +1,48 @@
+ 
+
+function ProductList() {
+    return (
+        <section>
+               <Title />
+            <Product />
+
+            <Product />
+        </section>
+    );
+}
+
+const Title = () => {
+    return <h2>Main Page</h2>;
+}
+
+const Description = () => {
+    return <h1> Pedigree dog food </h1>;
+}
+
+const Product = () => {
+    return( 
+    <article> 
+
+    <Image></Image> 
+     <Description />
+    </article>
+    
+    );
+}
+
+const Image = () => (
+<img 
+    src= 'https://images-na.ssl-images-amazon.com/images/I/81DsEA4QyyL.__AC_SX300_SY300_QL70_FMwebp_.jpg' alt='' /> );
+
+
 const MainPage = (props) => {
-    return <h1>I am the main page</h1>;
+    return (
+    
+    <ProductList />
+        
+    );
 };
+ 
+ 
 
 export default MainPage;
