eb691f40e0c6a05f482f9373153aba1025f16326
yennie0819 <anpeteran2@gmail.com>
2022-09-29 20:17:46 +1300

    Added MUI and React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import './App.css';
-import AccountPage from './components/AccountPage';
+// Library import
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import NavBar from "./components/Navigation/NavBar";
+
+// Screen import
+import MainPage from "./screens/MainPage";
+import AccountPage from "./screens/AccountPage";
+import DescriptionPage from "./screens/DescriptionPage";
+import NotFoundPage from "./screens/NotFoundPage";
 
 function App() {
-  return (
-    
-    <AccountPage />
-
-    
-  );
+    return (
+        <>
+            <NavBar />
+            <main>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/account" element={<AccountPage />} />
+                        <Route
+                            path="/description"
+                            element={<DescriptionPage />}
+                        />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </Router>
+            </main>
+        </>
+    );
 }
 
 export default App;
---
+++ src/screens/DescriptionPage.js
@@ -0,0 +1,5 @@
+const DescriptionPage = (props) => {
+  return <h1>I am the description page</h1>;
+};
+
+export default DescriptionPage;
