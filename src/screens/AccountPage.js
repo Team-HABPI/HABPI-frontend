0e55bd93ebf35750e81024d74e66e216e4ce198d
aashkul <aashish.kulkarni33@gmail.com>
2022-10-01 13:21:45 +1300

    AccountPage.js padding fixed

--- src/screens/AccountPage.js
+++ src/screens/AccountPage.js
@@ -28,39 +28,39 @@ const AccountPage = () => {
 
         <>
             <Container padding="normal">
-            {loadedUser && (
-                <Typography variant="h2" sx={{pl:3}}>{loadedUser.name}</Typography>
+                {loadedUser && (
+                    <Typography variant="h2" sx={{ pl: 3, p: 4 }}>{loadedUser.name}</Typography>
 
-            )
-            }
+                )
+                }
 
-            <Grid
-             container item justifyContent="flex"
-            >
-                
+                <Grid
+                    container item justifyContent="flex" style={{ alignContent: 'end' }}
+                >
+
                     {!loadedUser && <p>Loading or error</p>}
                     {loadedUser && (
 
-                        <Card sx={{ p: 3, m: 3 }}>
-                            <Typography variant="h3">Details</Typography>
-                            <Typography>Age: {loadedUser.age}</Typography>
-                            <Typography>Gender: {loadedUser.gender}</Typography>
-                            <Typography>Email: {loadedUser.email}</Typography>
+                        <Card sx={{ p: 6, m: 3 }}>
+                            <Typography variant="h3" sx={{ p: 1 }}>Details</Typography>
+                            <Typography sx={{ p: 0.6, m: 0.7 }}>Age: {loadedUser.age}</Typography>
+                            <Typography sx={{ p: 0.6, m: 0.7 }}>Gender: {loadedUser.gender}</Typography>
+                            <Typography sx={{ p: 0.6, m: 0.7 }}>Email: {loadedUser.email}</Typography>
 
 
                         </Card>
 
                     )}
-                <Button variant="contained"> Edit </Button>
+
                     {loadedUser && (
 
-                        <Card sx={{ p: 3, m: 2 }}>
-                            <Typography variant="h3">Pets</Typography>
+                        <Card sx={{ p: 5, m: 3, flexDirection: 'column' }}>
+                            <Typography variant="h3" sx={{ p: 2 }}>Pets</Typography>
 
-                            <Box sx={{ display: 'flex', flexDirection: "row" }}>
+                            <Box sx={{ display: 'flex', flexDirection: "row", p: 3 }}>
                                 {loadedUser.pets.map((pet) => {
                                     return (
-                                        <Card sx={{ pr: 2, mr: 3, minWidth: 120, boxShadow: "none" }}>
+                                        <Card sx={{ pr: 2, mr: 4, minWidth: 120, boxShadow: "none" }}>
 
                                             <Typography variant="h4">{pet.name}</Typography>
                                             <Typography >Age: {pet.age}</Typography>
@@ -77,8 +77,10 @@ const AccountPage = () => {
 
                     )
                     }
-            </Grid >
-        </Container>
+
+                </Grid >
+                <Button variant="contained" sx={{ p: 2, m: 3 }}> Edit </Button>
+            </Container>
 
         </>
     );
